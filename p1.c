db6bd058c94ea73ee1c1814fd85090b392730d9a
shreya-veni <shreyaveni17@gmail.com>
2025-09-15 22:29:44 +0530

    Add files via upload
    Fibonacci sequence! It shouldn't have taken me so long to figure out the logic but I finally did! Felt so accomplished!

---
+++ p1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// printing the fibonacci sequence
+int main(){
+    printf ("Let us print the Fibonacci sequence:\n");
+    int num1=1;
+    int num0 =0;
+    int i;
+    printf ("%d\n", num0);
+    printf ("%d\n", num1);
+    for (i = 1;i<100;i=i+num0){
+        printf ("%d\n", i);
+        num0 = num1;
+        num1 = i;
+    }
+    return 0;
+}
